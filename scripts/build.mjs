3caf10a4f25b433de73b04e9d3b397c8feb2eda2
kaonasi-biwa <85387717+kaonasi-biwa@users.noreply.github.com>
2025-09-06 17:19:22 +0900

    Add command to build for Firefox

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,7 +1,15 @@
-import { cp } from "node:fs/promises";
+import { cp, readFile, writeFile } from "node:fs/promises";
 import { build } from "esbuild";
 
-await cp("manifest.json", "dist/manifest.json");
+const manifest = JSON.parse(await readFile("manifest.json"))
+if (process.argv[2] === "firefox") {
+    manifest.browser_specific_settings = {
+        "gecko": {
+            "id": "twitterBookmarkDownloader@kotone.fami"
+        }
+    }
+}
+writeFile("dist/manifest.json", JSON.stringify(manifest))
 
 await build({
     entryPoints: ["src/index.ts"],
